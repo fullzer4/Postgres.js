125f66ccf494ca014650c819091914fcc1771387
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2022-10-26 15:17:29 -0300

    att

--- reconhecimento/voz.py
+++ reconhecimento/voz.py
@@ -1,13 +1,20 @@
-from email.mime import audio
-from tkinter import W
+import time
 import speech_recognition as sr
+
+global frase, mic
 
 rec = sr.Recognizer()
 
-print("\n",sr.Microphone().list_microphone_names())
-with sr.Microphone(device_index=8) as mic:
-    rec.adjust_for_ambient_noise(mic)
-    print("fale alguma coisa")
-    audio = rec.listen(mic)
-    frase = rec.recognize_google(audio, language="pt-BR")
-    print(frase)
+def IniciarDesk():
+    with sr.Microphone(device_index=8) as mic:
+        rec.adjust_for_ambient_noise(mic)
+        print("fale alguma coisa")
+        audio = rec.listen(mic)
+        frase = rec.recognize_google(audio, language="pt-BR")
+        print(frase)
+    time.sleep(5)
+    if "Gabriel" in frase:
+        print("foi")
+
+while True:
+    IniciarDesk()
