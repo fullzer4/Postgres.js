3685ca13a54b8d64e56ef4fee47519742e7591dd
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2022-10-26 11:52:54 -0300

    att

--- reconhecimento/voz.py
+++ reconhecimento/voz.py
@@ -1,11 +1,13 @@
+from email.mime import audio
 from tkinter import W
 import speech_recognition as sr
 
 rec = sr.Recognizer()
 
-with sr.Microphone(device_index=3) as mic:
+print("\n",sr.Microphone().list_microphone_names())
+with sr.Microphone(device_index=8) as mic:
     rec.adjust_for_ambient_noise(mic)
-    print("Reconhecimento de voz")
+    print("fale alguma coisa")
     audio = rec.listen(mic)
     frase = rec.recognize_google(audio, language="pt-BR")
     print(frase)
