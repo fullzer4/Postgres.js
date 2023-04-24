943a412257633318bdfef9929013bb232b835060
fullzer4 <gabrielpelizzaro@gmail.com>
2023-04-24 15:40:21 +0000

    new

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,8 +1,21 @@
-import { View } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 const app = () => {
     return(
-        <View>
+        <View style={styles.container}>
+            <Text>OpheliaAI</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#050505',
+        alignItems: 'center',
+        justifyContent: "space-between",
+        paddingBottom: 25,
+    }
+})
+
+export default app
